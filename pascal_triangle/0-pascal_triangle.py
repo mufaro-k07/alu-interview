31f6127f644bfdcf52ca8b5b8b59b34f32956d21
mufaro-k07 <m.kunze@alustudent.com>
2026-02-28 20:38:45 +0200

    Updated Pascal's Trinagle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-0-pascal_triangle
-"""
+"""Module that defines pascal_triangle function."""
 
-def pascal_triagnle(n):
+
+def pascal_triangle(n):
     """
     Returns a list of lists of integers representing
     Pascal's triangle of n
